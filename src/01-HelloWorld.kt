15ec187f831bde1c44ff67995b3358fe8e8b4925
Paras Sidhu <sidhuparas1234@gmail.com>
2019-07-03 11:44:57 +0530

    Initialized

---
+++ src/01-HelloWorld.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT. */
+    val scanner = Scanner(System.`in`)
+    val inputString = scanner.nextLine()
+    println("Hello, World.")
+    println(inputString)
+}
